7739d565b6e9b058e38f54ca584a84dfc3db9dfa
agmas <Maxlion360@gmail.com>
2024-12-05 21:59:03 +0400

    attribute fix, jdk17

--- src/main/java/org/agmas/scythes/items/Scythe.java
+++ src/main/java/org/agmas/scythes/items/Scythe.java
@@ -48,16 +48,15 @@ public class Scythe extends ToolItem implements PolymerItem, PolymerKeepModel, P
         modelData = PolymerResourcePackUtils.requestModel(item, Identifier.of("scythes", "item/"+modelName));
         toolMaterial = material;
         ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
-        builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Weapon modifier", (double)4f+toolMaterial.getAttackDamage(), EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Weapon modifier", (double)4f+toolMaterial.getAttackDamage()+bonusDamage, EntityAttributeModifier.Operation.ADDITION));
         builder.put(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Weapon modifier", (double)-3.2f, EntityAttributeModifier.Operation.ADDITION));
         builder.put(ReachEntityAttributes.REACH, new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Weapon modifier", (double)2.6f, EntityAttributeModifier.Operation.ADDITION));
         this.attributeModifiers = builder.build();
     }
     @Override
     public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
-        return attributeModifiers;
+        return slot == EquipmentSlot.MAINHAND ? this.attributeModifiers : super.getAttributeModifiers(slot);
     }
-
 
 
     @Override
